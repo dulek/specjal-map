ed0f0f88663bb0d4c979fd16dace4dfb96eb17d2
Michał Dulko <mdulko@redhat.com>
2020-04-04 14:39:41 +0200

    Added several photos
    Also changed map style to some black-and-white thing.

--- markers.js
+++ markers.js
@@ -1,8 +1,272 @@
 markers = [
   {
-    latitude: 52.64,
-    longitude: 42.39,
+    latitude: 41.403164,
+    longitude: 2.173260,
     photo: "photos/sagradafamilia.jpg",
-    title: "Sagrada Familia - Barcelona"
+    title: "Sagrada Familia - Barcelona - Spain"
+  },
+  {
+    latitude: 52.358408,
+    longitude: 4.883081,
+    photo: "photos/amsterdam.jpg",
+    title: "Rijksmuseum - Amsterdam - Netherlands"
+  },
+  {
+    latitude: 33.760179,
+    longitude: -84.374328,
+    photo: "photos/atlanta.jpg",
+    title: "Jackson Street Bridge - Atlanta - Georgia, USA"
+  },
+  {
+    latitude: 52.516360,
+    longitude: 13.378772,
+    photo: "photos/berlin.jpg",
+    title: "Brandenburger Tor - Berlin - Germany"
+  },
+  {
+    latitude: 44.430993,
+    longitude: 26.087265,
+    photo: "photos/bucaresti.jpg",
+    title: "Palatul Parlamentului - Bucharest - Romania"
+  },
+  {
+    latitude: 41.419353,
+    longitude: 2.161977,
+    photo: "photos/bunkers.jpg",
+    title: "Bunkers del Carmel - Barcelona - Spain"
+  },
+  {
+    latitude: 28.211452,
+    longitude: -14.018584,
+    photo: "photos/canaria.jpg",
+    title: "Playa Gan Tarajal - Fuerteventura - Canary Islands, Spain"
+  },
+  {
+    latitude: 38.780462,
+    longitude: -9.498610,
+    photo: "photos/capodelaroca.jpg",
+    title: "Cabo da Roca - Portugal"
+  },
+  {
+    latitude: 27.817559,
+    longitude: -97.389722,
+    photo: "photos/carrier.jpg",
+    title: "USS Lexington - Corpus Christi - Texas, USA"
+  },
+  {
+    latitude: 45.807043,
+    longitude: 9.070160,
+    photo: "photos/como.jpg",
+    title: "Como Lake - Italy"
+  },
+  {
+    latitude: 39.756244,
+    longitude: -105.005395,
+    photo: "photos/denver.jpg",
+    title: "Denver - Colorado, USA"
+  },
+  {
+    latitude: 53.342761,
+    longitude: -6.288293,
+    photo: "photos/dublin.jpg",
+    title: "Guiness Brewery - Dublin - Ireland"
+  },
+  {
+    latitude: 36.132295,
+    longitude: -5.348805,
+    photo: "photos/gibraltar.jpg",
+    title: "The Rock - Gibraltar"
+  },
+  {
+    latitude: 34.130865,
+    longitude: -118.318469,
+    photo: "photos/hollywood.jpg",
+    title: "Hollywood - Los Angeles - California, USA"
+  },
+  {
+    latitude: 53.400019,
+    longitude: -2.988467,
+    photo: "photos/liverpool.jpg",
+    title: "Liverpool - United Kingdom"
+  },
+  {
+    latitude: 38.693557, 
+    longitude: -9.206076,
+    photo: "photos/lizbon.jpg",
+    title: "Padrão dos Descobrimentos - Lisbon - Portugal"
+  },
+  {
+    latitude: 42.476291,
+    longitude: -2.434660,
+    photo: "photos/logrono.jpg",
+    title: "Logroño - Spain"
+  },
+  {
+    latitude: 40.416213,
+    longitude: -3.714302,
+    photo: "photos/madrid.jpg",
+    title: "Palacio Real - Madrid - Spain"
+  },
+  {
+    latitude: 43.740171,
+    longitude: 7.429586,
+    photo: "photos/monaco.jpg",
+    title: "Monaco - Spain"
+  },
+  {
+    latitude: 35.768166,
+    longitude: -5.935260,
+    photo: "photos/morocco.jpg",
+    title: "Morocco"
+  },
+  {
+    latitude: 48.137527,
+    longitude: 11.574891,
+    photo: "photos/munich.jpg",
+    title: "Marienplatz - Munich - Germany"
+  },
+  {
+    latitude: 29.552022,
+    longitude: -95.096203,
+    photo: "photos/nasa.jpg",
+    title: "Johnson Space Center - Houston - Texas, USA"
+  },
+  {
+    latitude: 35.896443,
+    longitude: -78.941505,
+    photo: "photos/nc.jpg",
+    title: "Raleigh - North Carolina, USA"
+  },
+  {
+    latitude: 43.722566,
+    longitude: 10.395771,
+    photo: "photos/piza.jpg",
+    title: "Duomo - Pisa - Italy"
+  },
+  {
+    latitude: 50.086435,
+    longitude: 14.412275,
+    photo: "photos/prague.jpg",
+    title: "Karlův most - Prague - Czechia"
+  },
+  {
+    latitude: 37.805399,
+    longitude: -122.452788,
+    photo: "photos/san-francisco.jpg",
+    title: "Golden Gate - San Francisco - California, USA"
+  },
+  {
+    latitude: 43.936623,
+    longitude: 12.446718,
+    photo: "photos/san-marino.jpg",
+    title: "Piazza della Libertà - San Marino"
+  },
+  {
+    latitude: 31.24016,
+    longitude: 121.48602,
+    photo: "photos/shanghai.jpg",
+    title: "The Bund - Shanghai - China"
+  },
+  {
+    latitude: 36.716964,
+    longitude: -3.544821,
+    photo: "photos/spain.jpg",
+    title: "Playa de Granada - Motril - Spain"
+  },
+  {
+    latitude: 35.629198,
+    longitude: 139.733032,
+    photo: "photos/tokyo.jpg",
+    title: "Tokyo - Japan"
+  },
+  {
+    latitude: 49.290334,
+    longitude: -123.120247,
+    photo: "photos/vancouver.jpg",
+    title: "Vancouver - Canada"
+  },
+  {
+    latitude: 45.437961,
+    longitude: 12.335883,
+    photo: "photos/venice.jpg",
+    title: "Ponte di Rialto - Venice - Italy"
+  },
+  {
+    latitude: 53.962732,
+    longitude: -1.084979,
+    photo: "photos/york.jpg",
+    title: "City Wall - York - United Kingdom"
+  },
+  {
+    latitude: 55.289168,
+    longitude: 14.782448,
+    photo: "photos/bornholm.jpg",
+    title: "Allinge - Bornholm - Denmark"
+  },
+  {
+    latitude: 37.181038,
+    longitude: -3.592681,
+    photo: "photos/granada.jpg",
+    title: "Alhambra - Granada - Spain"
+  },
+  {
+    latitude: 45.524681,
+    longitude: 13.583396,
+    photo: "photos/piran.jpg",
+    title: "Casa Stelmach - Piran - Slovenia"
+  },
+  {
+    latitude: 35.902282,
+    longitude: 14.514344,
+    photo: "photos/malta.jpg",
+    title: "Valetta - Malta"
+  },
+  {
+    latitude: 54.024725,
+    longitude: 23.978232,
+    photo: "photos/druskienniki.jpg",
+    title: "Druskininkai - Litwa"
+  },
+  {
+    latitude: 54.415725,
+    longitude: 18.567558,
+    photo: "photos/dickmana.jpg",
+    title: "Gdańsk - Poland"
+  },
+  {
+    latitude: 42.508728,
+    longitude: 1.529388,
+    photo: "photos/andorra.jpg",
+    title: "Andorra la Vella - Andorra"
+  },
+  {
+    latitude: 49.194755,
+    longitude: 16.608632,
+    photo: "photos/brno.jpg",
+    title: "Brněnský orloj - Brno - Czechia"
+  },
+  {
+    latitude: 37.015873,
+    longitude: -7.935581,
+    photo: "photos/faro.jpg",
+    title: "Faro - Portugal"
+  },
+  {
+    latitude: 3.158163,
+    longitude: 101.713305,
+    photo: "photos/kuala-lumpur.jpg",
+    title: "Petronas Towers - Kuala Lumpur - Malaysia"
+  },
+  {
+    latitude: 37.055651,
+    longitude: -3.304463,
+    photo: "photos/siete-lagunas.jpg",
+    title: "Sierra Nevada - Spain"
+  },
+  {
+    latitude: 35.806419,
+    longitude: 14.517013,
+    photo: "photos/malta2.jpg",
+    title: "Malta"
   },
 ]
